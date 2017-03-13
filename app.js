50898928e68b7fa258573a387bb27c635cafd53a
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2017-03-13 14:35:41 +0100

    upsert and get books

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+var dbUrl = 'mongodb://localhost:27017/book-inventory';
 
 app.use(bodyParser.json());
 
@@ -9,12 +12,33 @@ function logger(req, res, next) {
   next();
 };
 
-app.get('/', logger, function (req, res) {
-  res.send('Hello World!');
+
+
+app.get('/stock', function (req, res) {
+  MongoClient.connect(dbUrl, function (err, db) {
+    db.collection('books').find({}).toArray(function (err, books) {
+      res.json(books);
+      db.close();
+    });
+  });
 });
 
+app.get('/stock/:id', logger, function (req, res) {
+  MongoClient.connect(dbUrl, function (err, db) {
+    db.collection('books').findOne({ isbn: req.params.id }, function (err, book) {
+      res.json(book);
+      db.close();
+    });
+  });
+
+});
 app.post('/stock', function (req, res) {
-  res.json({ isbn: req.body.isbn, count: req.body.count });
+  var bookEntry = { isbn: req.body.isbn, count: req.body.count };
+  MongoClient.connect(dbUrl, function (err, db) {
+    db.collection('books').updateOne({ isbn: bookEntry.isbn }, bookEntry, { upsert: true });
+    db.close();
+    res.json({ isbn: req.body.isbn, count: req.body.count });
+  });
 });
 
 app.use(function (req, res, next) {
