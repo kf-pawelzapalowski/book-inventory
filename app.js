471772f3a6bf2662dcd915d78da3f038e5eb1a32
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2017-03-13 15:33:06 +0100

    use promises instead of callbacks and fail db asap

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
 var dbUrl = 'mongodb://localhost:27017/book-inventory';
+var collection =
+  MongoClient
+    .connect(dbUrl,
+    {
+      db: {
+        bufferMaxEntries: 0
+      }
+    }).then(function (db) {
+      return db.collection('books');
+    });
+
 
 app.use(bodyParser.json());
 
@@ -12,33 +23,45 @@ function logger(req, res, next) {
   next();
 };
 
+var errorHandler = function (err, req, res, next) {
+  console.error(err);
+  err.status = err.status || 500;
+  res.status(err.status).send('Error: ' + err.status);
+  next();
+};
 
-
-app.get('/stock', function (req, res) {
-  MongoClient.connect(dbUrl, function (err, db) {
-    db.collection('books').find({}).toArray(function (err, books) {
+app.get('/stock', function (req, res, next) {
+  collection
+    .then(function (collection) {
+      return collection.find({}).toArray();
+    })
+    .then(function (books) {
       res.json(books);
-      db.close();
-    });
-  });
+    })
+    .catch(next);
 });
 
 app.get('/stock/:id', logger, function (req, res) {
-  MongoClient.connect(dbUrl, function (err, db) {
-    db.collection('books').findOne({ isbn: req.params.id }, function (err, book) {
+  collection
+    .then(function (collection) {
+      return collection.findOne({ isbn: req.params.id });
+    })
+    .then(function (book) {
       res.json(book);
-      db.close();
-    });
-  });
-
+    })
+    .catch(next);
 });
-app.post('/stock', function (req, res) {
+
+app.post('/stock', function (req, res, next) {
   var bookEntry = { isbn: req.body.isbn, count: req.body.count };
-  MongoClient.connect(dbUrl, function (err, db) {
-    db.collection('books').updateOne({ isbn: bookEntry.isbn }, bookEntry, { upsert: true });
-    db.close();
-    res.json({ isbn: req.body.isbn, count: req.body.count });
-  });
+  collection
+    .then(function (collection) {
+      return collection.updateOne({ isbn: bookEntry.isbn }, bookEntry, { upsert: true })
+    })
+    .then(function () {
+      res.json({ isbn: req.body.isbn, count: req.body.count });
+    })
+    .catch(next);
 });
 
 app.use(function (req, res, next) {
@@ -47,10 +70,6 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-app.use(function (err, req, res, next) {
-  console.error(err);
-  err.status = err.status || 500;
-  res.status(err.status).send('Error: ' + err.status);
-})
+app.use(errorHandler);
 
 module.exports = app;
