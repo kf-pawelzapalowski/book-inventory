b1b5c6cb273d5eb0882a5eb81f57e04da5d77310
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2017-03-14 12:57:15 +0100

    default route

--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,7 @@ module.exports = function (app, stockController) {
     app.get('/stock', stockController.getAll);
     app.get('/stock/:isbn', logger, stockController.getCount);
     app.post('/stock', stockController.stockUp);
+    app.get('/', function(req, res){
+        res.send('Hello world');
+    });
 };
